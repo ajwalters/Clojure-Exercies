642fb7401ae38d44f64dcd2c0974b5ba775739f8
Adam Walters <awalters@groupon.com>
2012-05-26 13:34:42 -0500

    Problem 1 - Parse a CSV
    Created a program to parse a CSV and return a list of maps with the
    contents.

---
+++ 01-csv-parser/csv-parser.clj
@@ -0,0 +1,23 @@
+(require '[clojure.string :as str])
+
+(def input-string
+  (str "first-name,last-name,age,profession\n"
+       "Bob,Jones,35,Programmer\n"
+       "Alice,Johnson,25,Designer\n"
+       "Faye,Peterson,40,Manager\n"
+       "Don,Donaldson,33,Designer\n"
+       "Claire,Classon,38,Programmer"))
+
+(defn try-my-parser [parser-fn]
+  (parser-fn (java.io.StringReader. input-string)))
+
+(defn parse-row [row]
+  (str/split  row #","))
+
+(defn extract-headers [header-row]
+  (map keyword (parse-row header-row)))
+
+(defn parse-csv [reader]
+  (let [lines (clojure.string/split-lines (slurp reader))
+        headers (extract-headers (first lines))]
+    (map #(zipmap headers (parse-row %)) (rest lines))))
